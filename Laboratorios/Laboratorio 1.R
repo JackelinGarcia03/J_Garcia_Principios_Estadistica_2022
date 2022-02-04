23beda70e43a01334831a21fbbcf904728d09d4b
jackelin <youremail@domain.com>
2022-02-03 20:24:11 -0600

    Laboratorio 1

---
+++ Laboratorios/Laboratorio 1.R
@@ -0,0 +1,75 @@
+# J_Garcia
+# 04/02/2022
+# Laboratorio_Semana_1
+
+
+# Gastos totales ----------------------------------------------------------
+
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+# Gastos de facturacion de telefonía celular
+
+ celular <- 300
+ transporte <- 240
+ comestibles <- 1527
+ gimnasio <- 400
+ alquiler <- 1500
+ otros <- 1833
+ 
+ gastos <- c(celular, transporte, comestibles, gimnasio, alquiler, otros)
+ 
+
+# Base de datos -----------------------------------------------------------
+
+ "gastos al mes" <- 5800
+ 
+ celular <- 300
+ 
+ transporte <- 240
+ 
+ comestibles <- 1527
+ 
+ gimnasio <- 400
+ 
+ alquiler <- 1500
+ 
+ otros <- 1833
+ 
+ "gastos" <- data.frame("gastos al mes", celular, transporte, comestibles, gimnasio, alquiler, otros)
+
+
+ mean(gastos$celular) 
+ 
+ mean(gastos$transporte)
+ 
+ mean(gastos$comestibles)
+
+ mean(gastos$gimnasio)
+ 
+ mean(gastos$alquiler)
+ 
+ mean(gastos$otros)
+ 
+ 
+
+# Columna de gasto total --------------------------------------------------
+
+ 
+  `gasto total` <- 5800
+  
+  `gasto total` <- 5800 * 5 
+  
+  `gasto total` <- 5800 * 10
+  
+  `gasto total`
+  
+   data.frame(5800 * 5)
+
+   data.frame(5800 * 10)
+   
+   
+ 
+ "df" <- data.frame(gastos = c(celular, transporte, comestibles, gimnasio, alquiler,otros), num = c(300, 240, 1527, 400, 1500, 1833))
+   
+                  
+                  
