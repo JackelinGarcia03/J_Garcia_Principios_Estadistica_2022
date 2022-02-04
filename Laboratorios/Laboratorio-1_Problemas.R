04ebaad07004dc7ce4fe89913c02c8b1920dbb14
jackelin <youremail@domain.com>
2022-02-03 22:48:03 -0600

    Laboratorio 1_Problemas

---
+++ Laboratorios/Laboratorio-1_Problemas.R
@@ -0,0 +1,101 @@
+# J_Garcia
+# 04/02/2022
+# Problema 1
+
+
+# Variables cualitativas o cuantitativas ----------------------------------
+
+
+ + :list: Nombre del Estudiante <- Ximena Garza
+ 
+ + :list: Fecha de Nacimiento <- 21/10/1995
+ 
+ +  :list: Edad <- c(22)
+
+ + :list: Direccion de Casa <- 1234 Av.Alamo
+ 
+ +  :list: Numero de Telefono" <- 510-123-4567
+ 
+ + :list: Area Principal de Estudio <- "Facultad de Ciencias Forestales
+ 
+ + :list: Grado de Año Universitario <- Ultimo año
+ 
+ + :list: Puntaje en la prueba de mitad de periodo <- 95
+ 
+ + :list: Calificacion General <- A
+ 
+ + :list: Tiempo <- 120
+ 
+ + :list: Numero de hermanos <- 1
+ 
+
+# Lista de estudiantes (Cuantitativas o Cualitativas)
+
+
+ + Nombre del estudiante <- Cualitativa
+ 
+ + Fecha de nacimiento <- Cuantitativa
+ 
+ + Edad <- Cuantitativa
+ 
+ + Direccion de casa  <- Cuantitativa
+ 
+ + Numero de telefono <- Cuantitativa
+ 
+ + Area principal de estudio <- Cualitativa
+ 
+ + Grado de año universitario <- Cuantitativa
+ 
+ + Puntaje en la prueba de mitad de periodo <- Cuantitativa
+ 
+ + Calificacion general <- Cualitativa
+ 
+ + Tiempo <- Cuantitativa
+ 
+ + Numero de hermanos <- Cuantitativa
+ 
+ 
+  
+  #Problema 2
+  
+
+# Base de datos -----------------------------------------------------------
+
+ Ba <- c(1:4)
+ DAP <- c(28, 20.5, 29.4, 24.9)
+ DC <- c(11, 8.6, 13.1, 12.9)
+ Sp <- c("Caoba", "Pino", "Cedro", "Álamo")
+ 
+ inventario <- data.frame(Ba, DAP, DC, Sp) 
+ 
+ mean(inventario$DAP)
+ 
+ mean(inventario$DC)
+
+
+ 
+ #Problema 3
+ 
+ RS <- c(1:5)
+ Op <- c(1, 2, 3, 4, 5)
+ "Ms <- c(Twitter, Correo electronico, Mensaje de texto, Facebook, Blog)"
+
+ #Comment
+ 
+ + :question: ¿Es esta una variable cuantitativa o cualitativa?
+   
+ + :pencil: Esta seria una variable cuantitativa ya que a traves de recoleccion y generacion de numeros y datos conocemos las opiniones personales expresadas, estas mismas señalando informacion sobre la expresion de opinones de forma electronica.
+  
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
