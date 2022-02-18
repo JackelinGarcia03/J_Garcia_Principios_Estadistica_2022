fb0e20f1975959e6928be44f11eef9729608150e
jackelin <youremail@domain.com>
2022-02-18 11:01:22 -0600

    HW_03

---
+++ Tareas/HW_03.R
@@ -0,0 +1,117 @@
+# Jackelin_Garcia
+# 18/02/2022
+# Tarea_3_Medidas de tendencia central
+
+
+# Problemas de Tarea ------------------------------------------------------
+
+# Problema_1
+x <- c(6, 4, 1, 3)
+ys <- c(1, 3, 4, 2)
+sum (x)
+sum (ys)
+prod (x)
+prod (ys)
+sum (x, ys)
+prod (x, ys)
+prod (x^2, ys^0.5)
+
+#Problema_2
+A <- c(80, 90, 90, 100)
+B <- c(60, 65, 65, 70, 70, 70, 75, 75,
+            80, 80, 80, 80, 80, 85, 100)
+# a) 
+     "la altura media mas alta seria del grup A, por que sus datos
+     son menores y los valores mayores"
+     
+# b) 
+     mean(A)
+     mean(B) 
+     
+# Porblema_3
+     + "se necesita una calificacion de 76 para obtener un 80"
+     
+     promedio80 <- c(87, 72, 85, 76)
+     mean (promedio80)
+     
+# Problema_4
+     "b) hay un total de 110 niños"
+
+      promedio2.2 <- 110/50
+      promedio2.2
+
+# Problema_5
+      
+       germinaciones <-  c(5, 6, 7, 8, 9)
+       "caja petri <- c(1, 3, 5, 3, 1)"
+       
+# Problema_6
+       
+       conjunto  <- c( 2 , 2 , 3 , 6 , 10 )
+       
+       # a) calcular moda, mediana y media
+       "significa (conjunto)"
+       median (conjunto)
+       mode <-  "función ( conjunto )"
+       "{ return (como.numeric(nombres(cuál.max(tabla( conjunto ))))))}"
+       mode ("set")
+       
+       #b ) Suma 5 a cada valor
+       conjunto1  <- c( 2 + 5 , 2 + 5 , 3 + 5 , 6 + 5 , 10 + 5 )
+       mean( conjunto1 )
+       median ( conjunto1 )
+       mode ( conjunto1 )
+       "set1"
+       
+       # c) 
+       
+       "¿cómo cree que la moda, la mediana y la media se ven afectadas 
+       cuando se agrega la misma constante a cada valor de datos en un conjunto?
+       
+       los resultados se ven en aumento."
+       
+       # d) 
+       
+       "Multiplica cada valor por 5"
+       
+       set2 <- c(2*5, 2*5, 3*5, 6*5, 10*5)
+       mean(set2)
+       median(set2)
+       mode(set2)
+       set2
+       
+       # e) 
+       
+       "¿cómo crees que la moda, la mediana y la media se ven afectadas cuando cada valor de datos en un conjunto se multiplica por la misma constante?
+       
+         los valores de los resultados aumentaron."
+       
+       
+       # Problema_7 
+       
+       "(0,1,2,3,4,5,6,7,8,9)"
+       
+       # a) 
+       
+       "Enumere cinco dígitos que tengan una mediana de 7 y una media de 7."
+       
+       conjunto1 <- c(5, 7, 7, 7, 9)
+       median(conjunto1)
+       mean(conjunto1)
+       conjunto2 <- c(5, 6, 7, 8, 9)
+       median(conjunto2)
+       mean(conjunto2)
+       
+       # b) 
+       
+       "Enumere cinco dígitos que tengan una mediana de 7 y una media 
+       inferior de 7."
+       
+       nums1 <- c(3, 4, 7, 8, 9)
+       median(nums1)  
+       mean(nums1)
+       nums2 <- c(4, 5, 7, 8, 9)
+       median(nums2)
+       mean(nums2)
+       
+       
