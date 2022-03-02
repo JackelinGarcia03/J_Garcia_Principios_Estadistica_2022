0c2958991ea90eb2e3bcc9a7f0f77f52257dcaca
Jackelin <jackygarcia19905@gmail.com>
2022-03-02 09:19:31 -0600

    clase_s7_d1

---
+++ Clases/Clase_Semana_7_d1.R
@@ -0,0 +1,23 @@
+# J_Garcia
+# 02/02/2022
+# Clase_S7_d1
+
+semilla <- read.csv("Clases/BaseDeDatos_estadistica.csv" , header = T)
+graf.sem <- boxplot(semilla$Peso_gr)
+
+#Identificar valor extremo mayor a 40
+which(semilla$Peso_gr >40)
+#revisar toda la fila
+semilla[524, ]
+
+#Indentificar semillas con valores menores al rango
+quantile(semilla$Peso_gr, 0.00)
+graf.sem$out 
+which(semilla$Peso_gr <=2.671)
+
+#Identificar los cuartiles
+quantile(semilla$Peso_gr, 0.25)
+quantile(semilla$Peso_gr, 0.5)
+quantile(semilla$Peso_gr, 0.75)
+quantile(semilla$Peso_gr, 1)
+
