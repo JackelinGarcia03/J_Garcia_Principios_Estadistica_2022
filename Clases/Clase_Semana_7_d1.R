fff4cd8e30044f4d25bdcbaa8284a64a4a61e61e
Jackelin <jackygarcia19905@gmail.com>
2022-03-03 08:12:51 -0600

    Semana7_d2

--- Clases/Clase_Semana_7_d1.R
+++ Clases/Clase_Semana_7_d1.R
@@ -21,3 +21,7 @@ quantile(semilla$Peso_gr, 0.5)
 quantile(semilla$Peso_gr, 0.75)
 quantile(semilla$Peso_gr, 1)
 
+# Tablas de frecuencia para semillas
+
+numero <-  table(semilla$Cond)
+round(numero/6,1)
