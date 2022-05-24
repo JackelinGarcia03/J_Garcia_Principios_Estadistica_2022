d89e688ffd03a43777806d894759da7600673c4e
jackelin <youremail@domain.com>
2022-05-24 18:11:14 -0500

    Clase_Semana_4_d1_correción

---
+++ Clases/Clase_Semana_4_d1_correción.R
@@ -0,0 +1,22 @@
+# J_Garcia
+# 09/02/2022
+# Sesion_Semana_4_Sala_de_computo
+
+# Importar datos ----------------------------------------------------------
+vivero <- read.csv("Clases/BD_vivero.csv" , header = T)
+
+# Estadística descriptiva -------------------------------------------------
+
+# Medidas de tendencia central
+
+mean(vivero$IE)
+median(vivero$IE)
+range(vivero$IE)
+fivenum(vivero$IE)
+boxplot(vivero$IE , horizontal = TRUE , col= "red")   
+hist(vivero$IE , ylim = c(0,12))
+
+table(vivero$IE)
+ftable(vivero$IE)
+frec <- table(vivero$IE)     
+
